773a58695fd7c0cef23675e972953f8fdab946dd
liuzihao <developerlzh@gmail.com>
2019-11-09 15:37:51 +0800

    画圆圈

--- android/src/main/kotlin/me/yohom/amapbase/map/wave/MarkerWave.java
+++ android/src/main/kotlin/me/yohom/amapbase/map/wave/MarkerWave.java
@@ -41,9 +41,9 @@ public class MarkerWave {
                     @Override
                     public int compare(Circle o1, Circle o2) {
                         if (o1.getRadius() < o2.getRadius()) {
-                            return -1;
-                        } else if (o1.getRadius() > o2.getRadius()) {
                             return 1;
+                        } else if (o1.getRadius() > o2.getRadius()) {
+                            return -1;
                         } else {
                             return 0;
                         }
