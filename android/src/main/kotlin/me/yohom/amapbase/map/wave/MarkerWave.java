b6ed10031f5d0b9a995e1b43afc4c74f0ef71a5a
liuzihao <developerlzh@gmail.com>
2019-11-09 18:02:39 +0800

    画圆圈

--- android/src/main/kotlin/me/yohom/amapbase/map/wave/MarkerWave.java
+++ android/src/main/kotlin/me/yohom/amapbase/map/wave/MarkerWave.java
@@ -1,14 +1,14 @@
 package me.yohom.amapbase.map.wave;
 
 import android.animation.ValueAnimator;
+import android.graphics.Color;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class MarkerWave {
@@ -20,6 +20,9 @@ public class MarkerWave {
 
     private ValueAnimator valueAnimator;//动画工具
 
+    private int fillColor;
+    private int strokeColor;
+
     /**
      * 添加水波纹效果
      *
@@ -27,39 +30,42 @@ public class MarkerWave {
      *               AMap aMap：高德地图
      *               fillColor: #ffffff
      */
-    public void addWaveAnimation(LatLng latLng, AMap aMap, String fillColor,String strokeColor) {
+    public void addWaveAnimation(LatLng latLng, AMap aMap, String fill, String stroke) {
+        this.fillColor = Color.parseColor(fill);
+        this.strokeColor = Color.parseColor(stroke);
+
         circleList = new ArrayList<>();
-        int radius = 0;
-        for (int i = 0; i < 5; i++) {
-            radius = radius + 50 * i;
-            circleList.add(CircleBuilder.addCircle(latLng, radius, aMap, fillColor,strokeColor));
+        for (int i = 0; i < 4; i++) {
+            int radius = 50 * (i + 1);
+            circleList.add(addCircle(latLng, radius, aMap, fill, stroke));
         }
         valueAnimator = AnimatorUtil.getValueAnimator(0, 50, new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                Collections.sort(circleList, new Comparator<Circle>() {
-                    @Override
-                    public int compare(Circle o1, Circle o2) {
-                        if (o1.getRadius() < o2.getRadius()) {
-                            return 1;
-                        } else if (o1.getRadius() > o2.getRadius()) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    }
-                });
+                int value = (int) animation.getAnimatedValue();
                 for (int i = 0; i < circleList.size(); i++) {
+                    int nowradius = 50 + 50 * i;
                     Circle circle = circleList.get(i);
-                    int nowradius = (int) circle.getRadius();
-                    double radius1 = nowradius + 1;
-                    if (radius1 >= 250) {
-                        radius1 = 0;
-                    }
+                    double radius1 = value + nowradius;
                     circle.setRadius(radius1);
+                    int strokePercent;
+                    int fillPercent;
+                    if (value < 25) {
+                        strokePercent = value * 8;
+                        fillPercent = value * 20 / 50;
+                    } else {
+                        strokePercent = 200 - value * 4;
+                        fillPercent = 20 - value * 20 / 50;
+                    }
+                    circle.setStrokeColor(setArgb(strokePercent, strokeColor));
+                    circle.setFillColor(setArgb(fillPercent, fillColor));
                 }
             }
         });
+    }
+
+    public int setArgb(int alpha, int color) {
+        return Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
     }
 
     /**
@@ -75,5 +81,15 @@ public class MarkerWave {
             }
             circleList.clear();
         }
+    }
+
+    public Circle addCircle(LatLng latlng, double radius, AMap aMap, String fillColor, String strokeColor) {
+        CircleOptions options = new CircleOptions();
+        options.strokeWidth(1f);
+        options.fillColor(Color.parseColor(fillColor));
+        options.strokeColor(Color.parseColor(strokeColor));
+        options.center(latlng);
+        options.radius(radius);
+        return aMap.addCircle(options);
     }
 }
